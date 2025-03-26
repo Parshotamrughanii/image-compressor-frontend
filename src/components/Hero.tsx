d26a2c99eefc98398fb09e8fced19ebdfcbad4b0
parshotam786 <parshotamrughanii@gmail.com>
2025-03-26 17:43:31 +0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,7 +28,7 @@ let socket: Socket | null = null;
 
 // Only initialize socket on client side
 if (typeof window !== 'undefined') {
-  socket = io("https://www.compressclick.com/", {
+  socket = io("https://api.parshotamrughani.com/", {
     transports: ["websocket"], // Enforce WebSocket transport
     secure: true, // Ensure secure connection
     reconnection: true, // Auto-reconnect on disconnection
@@ -119,7 +119,7 @@ export default function Hero() {
       );
       
       // Send to server for compression
-      await fetch("https://www.compressclick.com/upload-single", {
+      await fetch("https://api.parshotamrughani.com/upload-single", {
         method: "POST",
         body: formData,
       });
